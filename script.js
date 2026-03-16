4c16c8bb6ae38119f64abf1bef63771b75fb78db
Thimira Vishwajith Bandara <Thimiravishwajith0927@gmail.com>
2026-03-16 22:28:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const apiKey = "176d2a6b";
 
 function searchMovie(){
 
-const movie = document.getElementById("movieInput").value;
+const movie = document.getElementById("movieInput").value.trim();
 
 const result = document.getElementById("movieResult");
 const loading = document.getElementById("loading");
@@ -11,12 +11,15 @@ const error = document.getElementById("error");
 error.innerHTML = "";
 result.innerHTML = "";
 
+if(movie === ""){
+error.innerHTML = "⚠️ Please enter a movie name";
+return;
+}
+
 loading.style.display = "block";
 
 fetch(`https://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`)
-
 .then(response => response.json())
-
 .then(data => {
 
 loading.style.display = "none";
@@ -24,10 +27,9 @@ loading.style.display = "none";
 if(data.Response === "True"){
 
 result.innerHTML = `
-
 <div class="movie-card">
 
-<img src="${data.Poster}">
+<img src="${data.Poster}" alt="Movie Poster">
 
 <h2>${data.Title}</h2>
 
@@ -50,11 +52,9 @@ error.innerHTML = "❌ Movie not found";
 }
 
 })
-
 .catch(err => {
 
 loading.style.display = "none";
-
 error.innerHTML = "⚠️ Error fetching movie data";
 
 });
