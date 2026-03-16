8d92816b855a006a37b80006e47784d6b3a59931
Thimira Vishwajith Bandara <Thimiravishwajith0927@gmail.com>
2026-03-16 23:16:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -45,6 +45,10 @@ result.innerHTML = `
 
 <p>${data.Plot}</p>
 
+<a href="https://www.youtube.com/results?search_query=${data.Title}+trailer"
+target="_blank"
+class="trailer-btn">▶ Watch Trailer</a>
+
 </div>
 `;
 
@@ -74,4 +78,93 @@ searchMovie();
 
 });
 
-/* TRENDING MOVIE*
+/* TRENDING MOVIES */
+
+function loadTrendingMovies(){
+
+const movies = [
+"avatar",
+"avengers",
+"doctor strange",
+"spiderman",
+"interstellar",
+"joker"
+];
+
+const container = document.getElementById("trendingMovies");
+
+container.innerHTML = "";
+
+movies.forEach(movie => {
+
+fetch(`https://www.omdbapi.com/?t=${movie}&apikey=${apiKey}`)
+.then(res => res.json())
+.then(data => {
+
+if(data.Response === "True" && data.Poster !== "N/A"){
+
+container.innerHTML += `
+
+<div class="trending-card" onclick="openMovie('${data.Title}')">
+
+<img src="${data.Poster}" alt="${data.Title}">
+
+<p>${data.Title}</p>
+
+</div>
+`;
+
+}
+
+});
+
+});
+
+}
+
+/* LOAD TRENDING MOVIES */
+
+loadTrendingMovies();
+
+/* OPEN MOVIE MODAL */
+
+function openMovie(title){
+
+fetch(`https://www.omdbapi.com/?t=${title}&apikey=${apiKey}`)
+.then(res => res.json())
+.then(data => {
+
+const modal = document.getElementById("movieModal");
+const details = document.getElementById("modalDetails");
+
+details.innerHTML = ` <img src="${data.Poster}">
+
+<h2>${data.Title}</h2>
+
+<div class="rating">⭐ IMDb ${data.imdbRating}</div>
+
+<p><b>Year:</b> ${data.Year}</p>
+<p><b>Genre:</b> ${data.Genre}</p>
+<p><b>Director:</b> ${data.Director}</p>
+<p><b>Actors:</b> ${data.Actors}</p>
+
+<p>${data.Plot}</p>
+
+<a href="https://www.youtube.com/results?search_query=${data.Title}+trailer"
+target="_blank"
+class="trailer-btn">▶ Watch Trailer</a>
+`;
+
+modal.style.display = "block";
+
+});
+
+}
+
+/* CLOSE MODAL */
+
+document.querySelector(".close").onclick = function(){
+
+document.getElementById("movieModal").style.display = "none";
+
+};
